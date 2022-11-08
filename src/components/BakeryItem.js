a3ea2713595220b7b429e0df274df3bd96bbcc3f
john <jfinberg@haverford.edu>
2022-11-08 14:04:24 -0500

    finished

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,22 @@
 // TODO: create a component that displays a single bakery item
+import React from 'react'
+
+function BakeryItem(props) {
+    const { item, addCart } = props
+
+    return (
+        <div className='BakeryItem'>
+            <h2 className='title'>{item.name}</h2>
+            <img className='BakeryImage' src={"./react-studio/"+item.image} alt={item.name} />
+            <p>{item.description}</p>
+            <br></br>
+            
+            <div className='option-wrapper'>
+                <div>${item.price}</div>
+                <div><a onClick={() => {addCart(item)}}>Add to Cart</a></div>
+            </div>
+        </div>
+    )
+}
+
+export default BakeryItem
